2452c5f5f78a05c34bb8831229ed4ff7ecaffb25
Divija3009 <divijayuvraj30@gmail.com>
2025-11-17 13:05:02 -0800

    Leetcode 1437

--- 1437/Solution.java
+++ 1437/Solution.java
@@ -1,24 +1,14 @@
 class Solution {
-    public boolean kLengthApart(int[] nums, int k) 
-    {   int dis=-1;
-        for(int i=0;i<nums.length;++i)
-        {
-            if(nums[i]==1)
-            {   
-                //check if they are at least k distance apart
-                if(dis==-1 || (i-dis-1)>=k)
-                {
-                    dis=i;
-                    continue;
-                }
-                else
-                {
+    public boolean kLengthApart(int[] nums, int k) {
+        int last = -1;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] == 1) {
+                if (last != -1 && i - last - 1 < k) {
                     return false;
                 }
-                
+                last = i;
             }
-        
-        }  
-        return true; 
+        }
+        return true;
     }
 }
