b027153980fe9ebfd4c6b092e5627d994179d63e
Divija3009 <divijayuvraj30@gmail.com>
2025-12-24 19:11:50 -0800

    Leetcode 3075

--- 3075/Solution.java
+++ 3075/Solution.java
@@ -11,13 +11,8 @@ class Solution {
         Arrays.sort(arr, Collections.reverseOrder());
 
         long ans = 0;
-
         for (int i = 0; i < k; i++) {
-            int val = arr[i] - i;
-            if (val > 0) 
-            {
-                ans += val;
-            }
+            ans += Math.max(arr[i] - i, 0);
         }
         return ans;
     }
